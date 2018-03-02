610d11b8c3ec0d5971ebd9f2146e7e2e0fc7b9c7
abruckman <abruckman09@gmail.com>
2018-03-01 20:15:44 -0800

    add github icon and fix links

--- src/Header.js
+++ src/Header.js
@@ -16,10 +16,16 @@ class Header extends Component {
         />
         <CardText>{profile.summary}</CardText>
         <CardText expandable={true}>
-          <div className="contact"><i className="material-icons">mail_outline</i> <a targent="_blank" href="{profile.email}">{profile.email}</a></div>
-          <div className="contact"><i className="material-icons">phone</i> {profile.phone} </div>
-          <div className="contact"> G <a target="_blank" href="{profile.github}">{profile.github}</a></div>
-          <div className="contact"> Li <a target="_blank" href="{profile.linkedin}">{profile.linkedin}</a></div>
+          <div className="contact">
+            <i className="material-icons">mail_outline</i>
+            <a targent="_blank" href={'//'+profile.email}>{profile.email}</a>
+          </div>
+          <div className="contact">
+            <i className="material-icons">phone</i>
+            {profile.phone}
+           </div>
+          <div className="contact"><a target="_blank" href={'//'+profile.github}> <img alt='github logo' src='GitHub-Mark-32px.png'/> {profile.github}</a></div>
+          <div className="contact"> Li <a target="_blank" href={"//" + profile.linkedin}>{profile.linkedin}</a></div>
           <div className="contact"></div>
           <div className="contact"></div>
         </CardText>
