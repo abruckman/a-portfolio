844a3ba3b7eb056b2e946254b6fb1a35856d2d1e
abruckman <abruckman09@gmail.com>
2018-03-05 20:33:00 -0800

    create employment card

--- src/Employment.js
+++ src/Employment.js
@@ -1,6 +1,26 @@
 import React, {Component} from 'react';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
+import Resume from './resume'
+
+
 
 class Employment extends Component{
-  
+  render(){
+    const history = Resume.employment
+    const jobs = history.map(job){
+      return(
+        <Job job={job}/>
+      )
+    }
+
+    return(
+      <Card>
+        <CardHeader>Experience</CardHeader>
+        <CardText expandable={true}>
+          {jobs}
+        </CarText>
+      </Card>
+    )
+  }
 }
+export default Employment
---
+++ src/Job.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import PropTypes from 'prop-types';
+
+
+class Job extends Component{
+
+  
+
+}
+
+Job.propTypes ={
+  job: proptype.object
+}
