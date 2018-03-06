35343ce98156cb14708f417304da3c85d5509d7f
abruckman <abruckman09@gmail.com>
2018-03-05 20:55:18 -0800

    add responsiblities

--- src/Job.js
+++ src/Job.js
@@ -6,6 +6,12 @@ import PropTypes from 'prop-types';
 class Job extends Component{
 
   render(){
+
+    const details = this.props.job.responsiblities.map( (responsiblity)=> {
+      return(
+        <li>{responsiblity}</li>
+      )
+    })
     return(
       <Card>
         <CardHeader
@@ -16,8 +22,11 @@ class Job extends Component{
         />
         <CardText expandable={true}>
          <div>{this.props.job.start_month + " "+this.props.job.start_year}
-              
+              {this.props.job.current ? " - now" : " - " + this.props.job.end_month + " " +this.props.job.end_year }
          </div>
+         <ul>
+          {details}
+         </ul>
         </CardText>
       </Card>
     )
